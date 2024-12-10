250ee6f4c42e5bd099f88a50606c82b61bb23f0b
BrendanBezuko <brendanbbezuko@gmail.com>
2024-12-09 21:48:58 -0700

    switched npm run generate to npx nuxi generate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: `@import "./assets/scss/_colors.scss";`,
+          additionalData: `@use "~/assets/scss/_colors.scss" as *;`,
         },
       },
     },
