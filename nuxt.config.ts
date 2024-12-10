cecd2bcde42973fd4a0159b874c96c1ec141936c
BrendanBezuko <brendanbbezuko@gmail.com>
2024-12-10 00:56:23 -0700

    added baseURL setting for github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,10 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/scss/main.scss"],
-
+  ssr: true,
+  app: {
+    baseURL: "/RidiculousButtons/", // Replace 'repository-name' with your GitHub repo name
+  },
   vite: {
     plugins: [
       {
